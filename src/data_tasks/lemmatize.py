999e2d6c9c48dce01b5bd4afd699ba70f082c33d
zolizoli <zoltan.varju@gmail.com>
2022-12-02 15:11:01 +0100

    add better and faster final processing

--- src/data_tasks/lemmatize.py
+++ src/data_tasks/lemmatize.py
@@ -1,21 +1,33 @@
 import asyncio
+from pathlib import Path
 
 import aiofiles
 import spacy
 
 
-async def process_file(data_file, out_file):
+async def process_files(in_path, out_path):
+    pathlist = Path(in_path).glob('*')
     nlp = spacy.load("hu_core_news_lg")
-    async with aiofiles.open(data_file, mode='r') as f:
-        async with aiofiles.open(out_file, mode="w") as outfile:
-            async for line in f:
-                lemmas = []
-                t = nlp(line)
-                for token in t:
-                    lemmas.append(token.lemma_.lower())
-                t = " ".join(lemmas)
-                await outfile.write(t)
 
+    for path in pathlist:
+        try:
+            async with aiofiles.open(f'{in_path}/{path.name}', mode='r') as infile:
+                lines = await infile.read()
+                lines = lines.split("\n")
+                ls = [s.strip() for s in lines]
+                ls = ".\n".join(ls)
+                t = nlp(ls)
+                lemmas = [token.lemma_.lower() for token in t]
+                size = len(lemmas)
+                idx_list = [idx + 1 for idx, val in enumerate(lemmas) if val == "."]
+                splitted = [lemmas[i: j - 1] for i, j in
+                            zip([0] + idx_list, idx_list + ([size] if idx_list[-1] != size else []))]
+                splitted = [" ".join(e).strip() for e in splitted]
+                splitted = "\n".join(splitted)
+            async with aiofiles.open(f'{out_path}/{path.name}_lemmas.txt', mode='w') as outfile:
+                await outfile.write(splitted)
+        except Exception as e:
+            print(e)
+            continue
 
-asyncio.run(process_file("data/interim/huwiki.txt", "data/processed/huwiki.txt"))
-print("Done")
+asyncio.run(process_files("data/interim/slices", "data/processed/wikipedia"))
