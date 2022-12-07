016b79764ada34a58f9db2e15c8527a05021f14b
zolizoli <zoltan.varju@gmail.com>
2022-12-07 10:05:52 +0100

    add open in colab badges

--- src/train/train_w2v.py
+++ src/train/train_w2v.py
@@ -1,7 +1,7 @@
 import gensim.models
 from gensim import utils
 
-corpus_path = "data/interim/oscar.txt"
+corpus_path = "data/interim/oscar_uncased.txt"
 
 
 class MyCorpus:
@@ -10,9 +10,10 @@ class MyCorpus:
     def __iter__(self):
         for line in open(corpus_path):
             # assume there's one document per line, tokens separated by whitespace
-            yield utils.simple_preprocess(line.lower().strip())
+            yield utils.simple_preprocess(line.strip())
 
 
 sentences = MyCorpus()
 model = gensim.models.Word2Vec(sentences=sentences, vector_size=300, window=5, min_count=10, workers=3)
 model.save("models/word2vec/oscar/oscarw2v.model")
+print("Done")
